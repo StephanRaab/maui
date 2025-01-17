4e22b0bab1514fcb185b235c6165e3ed086d4032
Stephan Raab <sarcartist@gmail.com>
2025-01-16 21:04:20 -0500

    start adding routing

--- Contacts/AppShell.xaml.cs
+++ Contacts/AppShell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Contacts
+﻿using Contacts.Views;
+
+namespace Contacts
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            // register routes
+            Routing.RegisterRoute(nameof(ContactsPage), typeof(ContactsPage));
+            Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
+            Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));
         }
     }
 }
---
+++ Contacts/Views/ContactsPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace Contacts.Views;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void AddContactClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void EditContactClicked(object sender, EventArgs e)
+    {
+
+    }
+}
